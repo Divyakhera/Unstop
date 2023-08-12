0e4cf171c57ace7f0a316df1511706e5c3b5e534
Divya Khera <divyakhera29@gmail.com>
2023-08-12 13:08:48 +0530

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,335 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <div class="abcd">
+        <div class="abcd-inner">
+          <div class="frame-parent8">
+            <div class="assessment-parent">
+              <div class="assessment">Assessment</div>
+              <img class="frame-child" alt="" src="./img/vector-298.svg" />
+
+              <div class="frame-wrapper5">
+                <div class="my-assessments-wrapper">
+                  <div class="my-assessments">My Assessments</div>
+                </div>
+              </div>
+            </div>
+            <div class="mobile-screen-share-wrapper" id="frameContainer3">
+              <img
+                class="mobile-screen-share-icon"
+                alt=""
+                src="./img/mobile-screen-share.svg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="abcd-child">
+          <div class="frame-wrapper6">
+            <div class="frame-parent9">
+              <div class="assessments-overview-parent">
+                <div class="assessments-overview">Assessments Overview</div>
+                <div class="frame-parent10">
+                  <div class="total-assessment-parent">
+                    <div class="assessment">Total Assessment</div>
+                    <div class="frame-parent11">
+                      <img
+                        class="frame-item"
+                        alt=""
+                        src="./img/frame-1000009100.svg"
+                      />
+
+                      <b class="b">34</b>
+                    </div>
+                  </div>
+                  <img class="frame-inner" alt="" src="./img/vector-251.svg" />
+
+                  <div class="candidates-parent">
+                    <div class="assessment">Candidates</div>
+                    <div class="frame-parent12">
+                      <img
+                        class="frame-item"
+                        alt=""
+                        src="./img/frame-1000009353.svg"
+                      />
+
+                      <div class="frame-parent13">
+                        <div class="parent">
+                          <b class="b">11,145</b>
+                          <div class="div">+89</div>
+                        </div>
+                        <div class="total-candidate">Total Candidate</div>
+                      </div>
+                      <img
+                        class="vector-icon"
+                        alt=""
+                        src="./img/vector-250.svg"
+                      />
+
+                      <div class="frame-parent13">
+                        <div class="parent">
+                          <b class="b">1,14</b>
+                          <div class="div">+89</div>
+                        </div>
+                        <div class="total-candidate">Who Attamped</div>
+                      </div>
+                    </div>
+                  </div>
+                  <img class="frame-inner" alt="" src="./img/vector-251.svg" />
+
+                  <div class="candidates-source-parent">
+                    <div class="assessment">Candidates Source</div>
+                    <div class="frame-parent12">
+                      <img
+                        class="frame-item"
+                        alt=""
+                        src="./img/frame-1000005832.svg"
+                      />
+
+                      <div class="frame-parent13">
+                        <div class="parent">
+                          <b class="b">11,000</b>
+                          <div class="div">+89</div>
+                        </div>
+                        <div class="total-candidate">E-mail</div>
+                      </div>
+                      <img
+                        class="vector-icon"
+                        alt=""
+                        src="./img/vector-250.svg"
+                      />
+
+                      <div class="frame-parent13">
+                        <div class="parent">
+                          <b class="b">145</b>
+                          <div class="div">+89</div>
+                        </div>
+                        <div class="total-candidate">Social Share</div>
+                      </div>
+                      <img
+                        class="vector-icon"
+                        alt=""
+                        src="./img/vector-250.svg"
+                      />
+
+                      <div class="frame-parent13">
+                        <div class="parent">
+                          <b class="b">145</b>
+                          <div class="div">+89</div>
+                        </div>
+                        <div class="total-candidate">Unique Link</div>
+                      </div>
+                    </div>
+                  </div>
+                  <img class="frame-inner" alt="" src="./img/vector-251.svg" />
+
+                  <div class="total-purpose-parent">
+                    <div class="assessment">Total Purpose</div>
+                    <div class="frame-parent11">
+                      <img
+                        class="frame-item"
+                        alt=""
+                        src="./img/frame-10000091001.svg"
+                      />
+
+                      <b class="b">11</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="assessments-overview-parent">
+                <div class="my-assessment-parent">
+                  <div class="assessments-overview">My Assessment</div>
+                  <div class="frame-wrapper7">
+                    <div class="frame-child7"></div>
+                  </div>
+                </div>
+                <div class="frame-parent21">
+                  <div class="frame-parent22">
+                    <div class="frame-parent23" id="frameContainer30">
+                      <div class="frame-parent24">
+                        <div class="add-wrapper">
+                          <img class="add-icon" alt="" src="./img/add.svg" />
+                        </div>
+                        <div class="new-assessment">New Assessment</div>
+                      </div>
+                      <div class="from-here-you">
+                        From here you can add questions of multiple types like
+                        MCQs, subjective (text or paragraph)!
+                      </div>
+                      <img class="vector-icon1" alt="" src="./img/vector.svg" />
+                    </div>
+                    <div class="frame-parent25">
+                      <div class="frame-parent26">
+                        <div class="frame-parent27">
+                          <img
+                            class="frame-child8"
+                            alt=""
+                            src="./img/frame-1000008703.svg"
+                          />
+
+                          <div class="math-assessment-parent">
+                            <div class="assessments-overview">
+                              Math Assessment
+                            </div>
+                            <div class="job-parent">
+                              <div class="job">Job</div>
+                              <img
+                                class="frame-child9"
+                                alt=""
+                                src="./img/vector-333.svg"
+                              />
+
+                              <div class="calendar-today-parent">
+                                <img
+                                  class="calendar-today-icon"
+                                  alt=""
+                                  src="./img/calendar-today.svg"
+                                />
+
+                                <div class="new-assessment">20 Apr 2023</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="dot-wrapper">
+                          <img
+                            class="dot-icon1"
+                            alt=""
+                            src="./img/3-dot1.svg"
+                          />
+                        </div>
+                      </div>
+                      <img
+                        class="frame-child10"
+                        alt=""
+                        src="./img/vector-332.svg"
+                      />
+
+                      <div class="frame-parent28">
+                        <div class="frame-parent29">
+                          <div class="parent3">
+                            <div class="assessment">00</div>
+                            <div class="duration">Duration</div>
+                          </div>
+                          <div class="parent3">
+                            <div class="assessment">00</div>
+                            <div class="duration">Questions</div>
+                          </div>
+                        </div>
+                        <div class="frame-parent30">
+                          <div class="link-parent">
+                            <img
+                              class="link-icon"
+                              alt=""
+                              src="./img/link.svg"
+                            />
+
+                            <div class="assessments-overview">Share</div>
+                          </div>
+                          <div class="frame-wrapper8">
+                            <div class="lp-wrapper">
+                              <b class="lp">LP</b>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="frame-child11"></div>
+                    </div>
+                    <div class="frame-parent25">
+                      <div class="frame-parent26">
+                        <div class="frame-parent27">
+                          <img
+                            class="frame-child8"
+                            alt=""
+                            src="./img/frame-1000008703.svg"
+                          />
+
+                          <div class="math-assessment-parent">
+                            <div class="assessments-overview">
+                              Math Assessment
+                            </div>
+                            <div class="job-parent">
+                              <div class="job">Job</div>
+                              <img
+                                class="frame-child9"
+                                alt=""
+                                src="./img/vector-333.svg"
+                              />
+
+                              <div class="calendar-today-parent">
+                                <img
+                                  class="calendar-today-icon"
+                                  alt=""
+                                  src="./img/calendar-today.svg"
+                                />
+
+                                <div class="new-assessment">20 Apr 2023</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="dot-wrapper">
+                          <img
+                            class="dot-icon1"
+                            alt=""
+                            src="./img/3-dot1.svg"
+                          />
+                        </div>
+                      </div>
+                      <img
+                        class="frame-child10"
+                        alt=""
+                        src="./img/vector-332.svg"
+                      />
+
+                      <div class="frame-parent28">
+                        <div class="frame-parent29">
+                          <div class="parent3">
+                            <div class="assessment">00</div>
+                            <div class="duration">Duration</div>
+                          </div>
+                          <div class="parent3">
+                            <div class="assessment">00</div>
+                            <div class="duration">Questions</div>
+                          </div>
+                        </div>
+                        <div class="frame-parent30">
+                          <div class="link-parent">
+                            <img
+                              class="link-icon"
+                              alt=""
+                              src="./img/link1.svg"
+                            />
+
+                            <div class="assessments-overview">Share</div>
+                          </div>
+                          <div class="frame-wrapper8">
+                            <div class="frame-parent37">
+                              <div class="lp-wrapper">
+                                <b class="lp">LP</b>
+                              </div>
+                              <div class="lp-frame">
+                                <b class="lp">LP</b>
+                              </div>
+                              <div class="lp-wrapper1">
+                                <b class="lp">LP</b>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="frame-child11"></div>
+                    </div>
+                  </div>
+                  <div class="frame-child16"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
